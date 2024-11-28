17d61abae8987b75010883def2c18185b6099392
Krzysztof Szymańki <1tiskrzszy>
2024-11-28 14:52:51 +0100

    1 wersja LED Miganie

---
+++ LED_miganie.cpp.ino
@@ -0,0 +1,118 @@
+const int ledPin13 = 13;
+const int ledPin12 = 12;
+const int ledPin11 = 11;
+const int ledPin10 = 10;
+const int ledPin9 = 9;
+const int ledPin8 = 8;
+const int ledPin7 = 7;
+const int ledPin6 = 6;
+const int ledPin5 = 5;
+const int ledPin4 = 4;
+const int ledPin3 = 3;
+const int ledPin2 = 2;
+const int ledPin1 = 1;
+const int ledPin0 = 0;
+
+void setup() {
+  pinMode(ledPin0, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin7, OUTPUT);
+  pinMode(ledPin8, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+  pinMode(ledPin10, OUTPUT);
+  pinMode(ledPin11, OUTPUT);
+  pinMode(ledPin12, OUTPUT);
+  pinMode(ledPin13, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(ledPin13, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin13, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin12, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin12, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin11, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin11, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin10, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin10, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin9, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin9, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin8, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin8, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin7, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin7, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin6, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin6, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin5, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin5, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin4, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin4, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin3, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin3, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin2, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin2, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin1, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin1, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin0, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin0, LOW);
+  delay(1000);
+}
